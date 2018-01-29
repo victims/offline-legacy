c1ee7f149f6f7f48362562124e02a39456586a12
Steve Milner <smilner@redhat.com>
2018-01-29 17:09:19 +0000

    Serve from server/ and forward to blog

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import os
 import http.server
 import socketserver
 
@@ -7,6 +8,7 @@ class redirectHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.do_redirect('remove')
         self.do_redirect('update')
+        self.go_to_blog()
         super(redirectHandler, self).do_GET()
 
     def do_redirect(self, path):
@@ -15,6 +17,15 @@ class redirectHandler(http.server.SimpleHTTPRequestHandler):
             self.send_header('Location', '/service/v2/%s/1970-01-01T00:00:00/' % path)
             self.end_headers()
 
+    def go_to_blog(self):
+        if self.path in ('/index.html', '/'):
+            self.send_response(301)
+            self.send_header('Location', 'https://blog.victi.ms')
+            self.end_headers()
+
+
+# Server from the server directory as it has the required files
+os.chdir('server')
 with socketserver.TCPServer(("", PORT), redirectHandler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
